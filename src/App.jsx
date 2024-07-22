7aadca3aca9962e9f07529c21544c2681fef6f93
Alcodic <avi211169@gmail.com>
2024-07-22 09:31:00 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import ContactForm from "./components/ContactForm";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Contact Form</h1>
+        <ContactForm />
+      </header>
+    </div>
+  );
+}
+
+export default App;
